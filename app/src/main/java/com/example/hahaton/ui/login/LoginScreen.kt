743f3bb1ddb98ba023d23002a9add0fc372be767
Aleksandr <musketaleksand41@gmail.com>
2025-09-17 16:19:23 +0600

    added registration screen buttons

--- app/src/main/java/com/example/hahaton/ui/login/LoginScreen.kt
+++ app/src/main/java/com/example/hahaton/ui/login/LoginScreen.kt
@@ -16,6 +16,8 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.google.firebase.Firebase
+import com.google.firebase.auth.EmailAuthCredential
+import com.google.firebase.auth.EmailAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.auth
 
@@ -31,6 +33,7 @@ fun LoginScreen(
     val passwordState = remember {
         mutableStateOf("")
     }
+    Log.d("MyLog", "User email: ${auth.currentUser?.email}")
     Column(
         modifier = Modifier.fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
@@ -38,38 +41,92 @@ fun LoginScreen(
     ){
         TextField(
             value = emailState.value,
-            onValueChange = {
-                emailState.value = it
-            }
+            onValueChange = { emailState.value = it },
+            label = { Text("Email") }
         )
 
         Spacer(modifier = Modifier.height(10.dp))
 
         TextField(
             value = passwordState.value,
-            onValueChange = {
-                passwordState.value = it
-            }
+            onValueChange = { passwordState.value = it },
+            label = { Text("Password") }
         )
 
         Spacer(modifier = Modifier.height(10.dp))
 
         Button(onClick = {
-            signUp(auth, emailState.value, passwordState.value)
-            onLoginSuccess()
+            signIn(auth, emailState.value, passwordState.value){
+                onLoginSuccess()
+            }
+        }){
+            Text(text = "Sign In")
+        }
+
+        Button(onClick = {
+            signUp(auth, emailState.value, passwordState.value){
+                onLoginSuccess()
+            }
         }){
             Text(text = "Sign Up")
         }
+
+        Button(onClick = {
+            signOut(auth)
+        }){
+            Text(text = "Sign Out")
+        }
+
+        Button(onClick = {
+            deleteAccount(auth,emailState.value,passwordState.value)
+        }){
+            Text(text = "Delete Account")
+        }
+
     }
 }
 
-private fun signUp (auth: FirebaseAuth, email: String, password: String){
+private fun signUp(auth: FirebaseAuth, email: String, password: String, onSuccess: () -> Unit) {
     auth.createUserWithEmailAndPassword(email, password)
         .addOnCompleteListener {
-            if (it.isSuccessful){
+            if (it.isSuccessful) {
                 Log.d("MyLog", "Sign Up successful!")
-            }else{
+                onSuccess() // ⚡ вызываем при успешной регистрации
+            } else {
                 Log.d("MyLog", "Sign Up failure!")
             }
         }
+}
+
+
+private fun signIn(auth: FirebaseAuth, email: String, password: String, onSuccess: () -> Unit) {
+    auth.signInWithEmailAndPassword(email, password)
+        .addOnCompleteListener {
+            if (it.isSuccessful) {
+                Log.d("MyLog", "Sign In successful!")
+                onSuccess() // ⚡ вызываем при успешном входе
+            } else {
+                Log.d("MyLog", "Sign In failure!")
+            }
+        }
+}
+private fun signOut(auth: FirebaseAuth) {
+    auth.signOut()
+}
+
+private fun deleteAccount(auth: FirebaseAuth, email: String, password: String) {
+    val credential = EmailAuthProvider.getCredential(email, password)
+    auth.currentUser?.reauthenticate(credential)?.addOnCompleteListener {
+        if(it.isSuccessful){
+            auth.currentUser?.delete()?.addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Log.d("MyLog", "Account was deleted!")
+                } else {
+                    Log.d("MyLog", "Failure delete account!")
+                }
+            }
+        }else{
+            Log.d("MyLog", "Failure reauthenticate!")
+        }
+    }
 }
